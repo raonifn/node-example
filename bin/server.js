f675e5662ee21e1e9664b59ac36b863f5bfc7ae2
raonifn@gmail.com <>
2012-09-22 19:01:58 +0000

    Teste

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,8 @@
 var http = require('http');
-var
+var http = require('http');
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type' : 'text/plain' });
-    res.end('Hello'); 
-});
+var router = new director.http.Router();
+
+var server = http.createServer(router.dispatch(req, res));
 
 server.listen(9990);
