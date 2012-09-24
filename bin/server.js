6e8ce55f1806220b60af87d4e7d6f271d7424b8a
Raoni Fortes Normanton <t_60904@montanha.aquarius.cpqd.com.br>
2012-09-24 16:28:07 -0300

    teste

--- bin/server.js
+++ bin/server.js
@@ -1,16 +1,47 @@
 var http = require('http');
 var director = require('director');
 
+var showAuthor = function() {
+    this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
+    this.res.end('showAuthor');
+}
+
+var onAuthor = function() {
+    console.info('onAuthor');
+}
+
+
+var showCover = function() {
+    this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
+    this.res.end('showCover');
+}
+
+var showPicture = function() {
+    this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
+    this.res.end('showPicture');
+}
+
 var routes = {
   '/authors': {
-    on: showAuthor,
-    '/books': { on: [showCover, showDescription] },
-    '/bios': { on: [showPicture, showBiography] }
+    get: showAuthor,
+    '/books': { on: [showCover] },
+    '/bios': { on: [showPicture] }
   }
 };
 
-var router = new director.http.Router(routes);
+var router = new director.http.Router();
+router.path('/entity', function() {
+    this.path(/\/(.*)/, function() {
+        this.get(function(val) {
+            this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
+            this.res.end(val);
+        });
+    });
+    this.get(showAuthor);
+});
 
-var server = http.createServer(router.dispatch(req, res));
+var server = http.createServer(function(req, res) {
+    router.dispatch(req, res);
+});
 
 server.listen(9990);
