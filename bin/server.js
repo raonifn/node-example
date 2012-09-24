a7acfb032b9007fcbbf5d7f2b70cb7c62929344e
raonifn@gmail.com <>
2012-09-24 20:37:39 +0000

    Teste

--- bin/server.js
+++ bin/server.js
@@ -1,47 +1,30 @@
 var http = require('http');
 var director = require('director');
+var mongo = require('../lib/mongodb');
 
-var showAuthor = function() {
-    this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
-    this.res.end('showAuthor');
-}
-
-var onAuthor = function() {
-    console.info('onAuthor');
-}
-
-
-var showCover = function() {
-    this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
-    this.res.end('showCover');
-}
-
-var showPicture = function() {
-    this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
-    this.res.end('showPicture');
-}
-
-var routes = {
-  '/authors': {
-    get: showAuthor,
-    '/books': { on: [showCover] },
-    '/bios': { on: [showPicture] }
-  }
+var collect = function(name) {
+  mongo.collect(name, function(err, collection) {    
+    collection.count({}, function(err, value) {
+      this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
+      this.res.end(JSON.stringify({entity: name, count: value}));
+    });
+  });
 };
 
 var router = new director.http.Router();
 router.path('/entity', function() {
-    this.path(/\/(.*)/, function() {
-        this.get(function(val) {
-            this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
-            this.res.end(val);
-        });
-    });
-    this.get(showAuthor);
+  this.path(/\/(.*)/, function() {
+    this.get(collect);
+  }); 
 });
 
 var server = http.createServer(function(req, res) {
+  try {
     router.dispatch(req, res);
+  } catch (err) {
+    this.res.writeHeader(500, { 'Content-Type': 'text/plain'});
+    this.res.end(JSON.stringify({error: err}));
+  }
 });
 
 server.listen(9990);
