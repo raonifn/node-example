b2e56249583c65a80e66866a6386c92f4b2f8e3f
Raoni Fortes Normanton <t_60904@montanha.aquarius.cpqd.com.br>
2012-09-25 10:42:41 -0300

    teste

--- bin/server.js
+++ bin/server.js
@@ -2,29 +2,45 @@ var http = require('http');
 var director = require('director');
 var mongo = require('../lib/mongodb');
 
-var collect = function(name) {
-  mongo.collect(name, function(err, collection) {    
-    collection.count({}, function(err, value) {
-      this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
-      this.res.end(JSON.stringify({entity: name, count: value}));
-    });
+var count = function(name, route) {
+  console.info('count ' + name);
+  mongo.count(name, function(err, value) {    
+    if (err) {
+        throw err;
+    }
+    route.res.writeHeader(200, { 'Content-Type': 'text/plain'});
+    route.res.end(JSON.stringify({entity: name, count: value}));
   });
 };
 
 var router = new director.http.Router();
+
+router.get('/', function() {
+    this.res.writeHeader(200, { 'Content-Type': 'text/plain'});
+    this.res.end(JSON.stringify({status: 'online'}));
+});
+
+router.get('/fail', function() {
+    this.res.emit('error', {err:'erro'});
+});
+
 router.path('/entity', function() {
-  this.path(/\/(.*)/, function() {
-    this.get(collect);
+  this.path(/\/(\w*)/, function() {
+    this.get(function(name){
+        count(name, this);
+    });
   }); 
 });
 
 var server = http.createServer(function(req, res) {
-  try {
-    router.dispatch(req, res);
-  } catch (err) {
-    this.res.writeHeader(500, { 'Content-Type': 'text/plain'});
-    this.res.end(JSON.stringify({error: err}));
-  }
+  res.on('error', function(err) {
+      res.writeHeader(500, { 'Content-Type': 'text/plain'});
+      res.end(JSON.stringify({error: err}));
+  });
+  router.dispatch(req, res, function (err) {
+      res.writeHeader(err.status, { 'Content-Type': 'text/plain'});
+      res.end(JSON.stringify(err));
+  });
 });
 
 server.listen(9990);
